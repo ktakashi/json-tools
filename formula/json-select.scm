fc0f5e2772f4d56e6b4580e537af587be06b7249
ktakashi <ktakashi@ymail.com>
2014-02-28 16:01:16 +0100

    added formula for pegasus

---
+++ formula/json-select.scm
@@ -0,0 +1,11 @@
+;; json-tools library formula
+(formula
+ (description "JSON tools for R6RS Scheme")
+ (version "HEAD")
+ (homepage :url "https://github.com/ktakashi/json-tools")
+ (author :name "Takashi Kato" :email "ktakashi@ymail.com")
+ (source 
+  :type zip
+  :url "https://github.com/ktakashi/json-tools/archive/master.zip")
+ (install (directories ("src"))))
+ 
